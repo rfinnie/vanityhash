b67379eaf4b87e7445d2f73a97c5150a8404d1f9
Ryan Finnie <ryan@finnie.org>
2026-05-01 10:46:21 -0700

    Bump requires-python; add explicit build-system; add stub setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# SPDX-PackageName: vanityhash
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://codeberg.org/rfinnie/vanityhash
+# SPDX-FileCopyrightText: © 2026 Ryan Finnie <ryan@finnie.org>
+# SPDX-License-Identifier: MPL-2.0
+
+from setuptools import setup
+
+setup()
